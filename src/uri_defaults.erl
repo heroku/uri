fce6ad3477fd813f3b42ccac2075107c926450ea
Geoff Cant <nem@erlang.geek.nz>
2012-05-09 17:09:23 -0700

    Adds functions for URI scheme defaults.
    Mainly schemes and ports for now.

---
+++ src/uri_defaults.erl
@@ -0,0 +1,47 @@
+%% @copyright Geoff Cant
+%% @author Geoff Cant <nem@erlang.geek.nz>
+%% @version {@vsn}, {@date} {@time}
+%% @doc URI Scheme defaults
+%% @end
+-module(uri_defaults).
+
+-export([scheme_defaults/0
+         ,default_port/1
+         ,default_port/2
+         ,is_default_port/2
+         ,is_default_port/3
+        ]).
+
+-spec scheme_defaults() ->
+    [{uri:scheme(), DefaultPort::inet:port_number()}].
+
+scheme_defaults() ->
+    [{http,  80}
+     ,{https, 443}
+     ,{ftp,   21}
+     ,{ssh,   22}
+     ,{sftp,  22}
+     ,{tftp,  69}
+     ,{syslog, 601}
+    ].
+
+default_port(Scheme) ->
+    default_port(Scheme, scheme_defaults()).
+
+default_port(Scheme, Schemes) ->
+    case proplists:get_value(Scheme, Schemes) of
+        Port when is_integer(Port) ->
+            Port;
+        undefined ->
+            {error, {unknown_scheme, Scheme}}
+    end.
+
+is_default_port(Scheme, Port) ->
+    is_default_port(Scheme, Port, scheme_defaults()).
+
+is_default_port(Scheme, Port, Schemes) ->
+    case default_port(Scheme, Schemes) of
+        Port -> default;
+        Other when is_integer(Other) -> non_default;
+        Err -> Err
+    end.
