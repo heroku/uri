e11ea45aa17bafa782451bc0e035b73517d1b928
Geoff Cant <nem@erlang.geek.nz>
2012-07-24 16:40:52 -0700

    Update tests to call uri:parse.

--- src/uri_parser_tests.erl
+++ src/uri_parser_tests.erl
@@ -6,39 +6,39 @@
 parse_test_() ->
     [ ?_assertMatch(#uri{scheme=http,
                          host = <<"[2010:836B:4179::836B:4179]">>, port=80, path = <<"/foobar.html">>},
-                    uri_parser2:parse(<<"http://[2010:836B:4179::836B:4179]/foobar.html">>, [{foo, false}])),
+                    uri:parse(<<"http://[2010:836B:4179::836B:4179]/foobar.html">>, [{foo, false}])),
       %% ?_assertMatch(#uri{scheme=http,
       %%                    host = <<"2010:836B:4179::836B:4179">>,port=80,path = <<"/foobar.html">>},
-      %%               uri_parser2:parse(<<"http://[2010:836B:4179::836B:4179]/foobar.html">>,
+      %%               uri:parse(<<"http://[2010:836B:4179::836B:4179]/foobar.html">>,
       %%                                 [{ipv6_host_with_brackets, false}])),
-      %% ?_assertError(malformed_url, uri_parser2:parse(<<"http://2010:836B:4179::836B:4179/foobar.html">>, [])),
+      %% ?_assertError(malformed_url, uri:parse(<<"http://2010:836B:4179::836B:4179/foobar.html">>, [])),
       %% ipv4
       ?_assertMatch(#uri{scheme=http,host = <<"127.0.0.1">>,port=80,path = <<"/foobar.html">>},
-                    uri_parser2:parse(<<"http://127.0.0.1/foobar.html">>, [])),
+                    uri:parse(<<"http://127.0.0.1/foobar.html">>, [])),
       %% host
       ?_assertMatch(#uri{scheme=http,host = <<"localhost">>,port=8888,path = <<"/foobar.html">>},
-                    uri_parser2:parse(<<"http://localhost:8888/foobar.html">>, [])),
+                    uri:parse(<<"http://localhost:8888/foobar.html">>, [])),
       %% Userinfo
       ?_assertMatch(#uri{scheme=http,userauth = <<"nisse:foobar">>,
                          host = <<"localhost">>,port=8888,path = <<"/foobar.html">>},
-                    uri_parser2:parse(<<"http://nisse:foobar@localhost:8888/foobar.html">>, [])),
+                    uri:parse(<<"http://nisse:foobar@localhost:8888/foobar.html">>, [])),
       %% Scheme error
       ?_assertError(no_scheme,
-                    uri_parser2:parse(<<"localhost/foobar.html">>, [])),
+                    uri:parse(<<"localhost/foobar.html">>, [])),
       ?_assertError(no_scheme,
-                    uri_parser2:parse(<<"localhost:8888/foobar.html">>, [])),
+                    uri:parse(<<"localhost:8888/foobar.html">>, [])),
 
       %% Query
       ?_assertMatch(#uri{scheme=http,host = <<"localhost">>,port=8888,path = <<"/foobar.html">>,q = <<"?foo=bar&foobar=42">>},
-                    uri_parser2:parse(<<"http://localhost:8888/foobar.html?foo=bar&foobar=42">>, [])),
+                    uri:parse(<<"http://localhost:8888/foobar.html?foo=bar&foobar=42">>, [])),
       %%  Esc chars
       ?_assertMatch(#uri{scheme=http,host = <<"www.somedomain.com">>,port=80,path = <<"/%2Eabc">>},
-                    uri_parser2:parse(<<"http://www.somedomain.com/%2Eabc">>, [])),
+                    uri:parse(<<"http://www.somedomain.com/%2Eabc">>, [])),
 
       ?_assertMatch(#uri{scheme=http,host = <<"www.somedomain.com">>,port=80,path = <<"/%252Eabc">>},
-                    uri_parser2:parse(<<"http://www.somedomain.com/%252Eabc">>, [])),
+                    uri:parse(<<"http://www.somedomain.com/%252Eabc">>, [])),
       ?_assertMatch(#uri{scheme=http,host = <<"www.somedomain.com">>,port=80,path = <<"/%25abc">>},
-                    uri_parser2:parse(<<"http://www.somedomain.com/%25abc">>, [])),
+                    uri:parse(<<"http://www.somedomain.com/%25abc">>, [])),
       ?_assertMatch(#uri{scheme=http,host = <<"www.somedomain.com">>,port=80,path = <<"/%25abc">>, q = <<"?foo=bar">>},
-                    uri_parser2:parse(<<"http://www.somedomain.com/%25abc?foo=bar">>, []))
+                    uri:parse(<<"http://www.somedomain.com/%25abc?foo=bar">>, []))
       ].
